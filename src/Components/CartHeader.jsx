813406eec3659e2bb3685bf90bd297a4c774b006
Kelvin-Joel <joelventura1907@gmail.com>
2024-08-30 08:20:42 -0500

    modifique algunas imagenes

--- src/Components/CartHeader.jsx
+++ src/Components/CartHeader.jsx
@@ -1,6 +1,6 @@
 import "../Styles/Header.css"
 const CartHeader = ({ShowModal,products}) => {
-  
+
   return (
     <header className="cart__header">
       <h1 className="card__header-logo">Ecommerce</h1>
@@ -8,6 +8,7 @@ const CartHeader = ({ShowModal,products}) => {
         <p className="header__amount-product">{products.length}</p>
         <img className="header__icon" src="./image/shopping-cart.png" alt="icon" />
       </div>
+     
     </header>
   )
 }
